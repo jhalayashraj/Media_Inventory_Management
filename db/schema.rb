91f4d6fd8989297405a6155603d219482466313d
Yash <jhalayashraj@gmail.com>
2018-04-21 01:11:05 +0530

    Added Layout #1
    - Updated to rails 5.2.0
    - Added Bootstrap, Font-awesome and Simple_form Gems
    - Created Layouts for Login, Signup and Application

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180211181023) do
+ActiveRecord::Schema.define(version: 2018_02_11_181023) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "notifications", force: :cascade do |t|
     t.integer "user_id"
